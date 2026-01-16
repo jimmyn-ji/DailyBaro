53c563fa3849f09769b826283b830a82db4b1976
user <2464344891@qq.com>
2026-01-17 03:26:21 +0800

    chore: 只保留 DailyBaro-cloud 目录，移除其他文件和音乐文件
    - 移除 DailyBaro-api、DailyBaro-app、DailyBaro-vue 目录
    - 移除音乐文件目录
    - 更新 .gitignore 排除音乐文件格式
    - 只保留 DailyBaro-cloud 微服务代码

--- DailyBaro-api/src/main/java/com/project/model/dto/CreateAnonymousPostDTO.java
+++
@@ -1,9 +0,0 @@
-package com.project.model.dto;
-
-import lombok.Data;
-
-@Data
-public class CreateAnonymousPostDTO {
-    private String content;
-    private String visibility; // "public" or "private"
-} 
--- DailyBaro-api/src/main/java/com/project/model/dto/CreateCommentDTO.java
+++
@@ -1,8 +0,0 @@
-package com.project.model.dto;
-
-import lombok.Data;
-
-@Data
-public class CreateCommentDTO {
-    private String content;
-}
